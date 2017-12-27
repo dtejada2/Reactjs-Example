72fde0c58bdad782629535229493fea94277df05
Daniel <danieltejada65@hotmail.com>
2017-12-27 10:51:43 -0400

    Creating stores

--- src/actions/authorActions.js
+++ src/actions/authorActions.js
@@ -2,16 +2,18 @@
 
 var Dispatcher = require('../dispatcher/appDispatcher');
 var AuthorApi = require('../api/authorApi');
+var ActionTypes = require('../constants/actionTypes');
 
 var AuthorActions = {
     createAuthor: function(author){
         var newAuthor = AuthorApi.saveAuthor(author);
 
+        //Hey dispatcher, go tell all the stores that an author was just created.
         Dispatcher.dispatch({
-            actionType: 
-            author:
-        })
+            actionType: ActionTypes.CREATE_AUTHOR,
+            author: newAuthor
+        });
     }
-}
+};
 
 module.exports = AuthorActions;
---
+++ src/stores/authorStore.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var Dispatcher = require('../dispatcher/appDispatcher');
+var ActionTypes = require('../constants/actionTypes');
+var EventEmitter = require('events').EventEmitter;
+var assign = require('object-assign');
+
+var AuthorStore = assign({}, EventEmitter)
