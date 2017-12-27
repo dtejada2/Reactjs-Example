0cd92593495fa2814197c7943150eaa696045fe6
Daniel <danieltejada65@hotmail.com>
2017-12-27 17:32:08 -0400

    continue with flux

--- src/stores/authorStore.js
+++ src/stores/authorStore.js
@@ -5,4 +5,16 @@ var ActionTypes = require('../constants/actionTypes');
 var EventEmitter = require('events').EventEmitter;
 var assign = require('object-assign');
 
-var AuthorStore = assign({}, EventEmitter)
+var AuthorStore = assign({}, EventEmitter.prototype, {
+    addChangeListener: function(callback){
+        this.on('change', callback);
+    },
+
+    removeChangeListener: function(callback){
+        this.removeChangeListener('change', callback);
+    },
+
+    emitChange: function(){
+        this.emit('change');
+    }
+})
